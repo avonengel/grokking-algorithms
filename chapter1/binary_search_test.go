16becaa78ade980ca1861e66cee0da557ff10175
Axel von Engel <a.vonengel@gmail.com>
2023-10-22 22:18:53 +0200

    first implementation of binary search

--- chapter1/binary_search_test.go
+++ chapter1/binary_search_test.go
@@ -7,20 +7,72 @@ import (
 
 func TestBinarySearch(t *testing.T) {
 	// given
-	input := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}
+	input := testInput(1, 16)
 
 	// when
-	index, error := binarySearch(input, 13)
+	index, err := binarySearch(input, 13)
 
 	// then
-	if error != nil {
-		t.Fatal(error)
+	if err != nil {
+		t.Fatal(err)
 	}
 	if index != 12 {
 		t.Errorf("got %d, wanted 12", index)
 	}
 }
 
+func testInput(lower int, upper int) []int {
+	result := make([]int, upper-lower+1)
+	for i := 0; i <= upper-lower; i++ {
+		result[i] = lower + i
+	}
+	return result
+}
+
+func TestBinarySearchSingleItemArray(t *testing.T) {
+	// given
+	input := []int{5}
+
+	// when
+	index, err := binarySearch(input, 5)
+
+	// then
+	if err != nil {
+		t.Fatal(err)
+	}
+	if index != 0 {
+		t.Errorf("got %d, wanted 0", index)
+	}
+}
+
+func TestNotInArray(t *testing.T) {
+	// given
+	input := testInput(1, 16)
+
+	// when
+	index, err := binarySearch(input, 42)
+	// then
+	if err == nil || err.Error() != "NotFound" {
+		t.Fatalf("Error NotFound expected, got (%d, %s)", index, err)
+	}
+}
 func binarySearch(input []int, item int) (int, error) {
-	return 0, errors.ErrUnsupported
+	low := 0
+	high := len(input) - 1
+	for {
+		if input[low] == item {
+			return low, nil
+		} else if input[high] == item {
+			return high, nil
+		}
+		if low+1 == high {
+			return 0, errors.New("NotFound")
+		}
+		mid := (high-low)/2 + low
+		if input[mid] <= item {
+			low = mid
+		} else {
+			high = mid
+		}
+	}
 }
