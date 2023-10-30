cee4bed2bcfe56e7d5d69ad608f2205a6f09f6bc
Axel von Engel <a.vonengel@gmail.com>
2023-10-30 21:48:35 +0100

    recursive sum

--- chapter4/sum.go
+++ chapter4/sum.go
@@ -3,6 +3,7 @@ package chapter4
 func sum(input []int) int {
 	if len(input) == 1 {
 		return input[0]
+	} else {
+		return input[0] + sum(input[1:])
 	}
-	return 0
 }
--- chapter4/sum_test.go
+++ chapter4/sum_test.go
@@ -23,3 +23,21 @@ func TestSumBaseCase(t *testing.T) {
 		})
 	}
 }
+
+func TestRecursive(t *testing.T) {
+	tests := []struct {
+		input    []int
+		expected int
+	}{
+		{[]int{1, 2}, 3},
+		{[]int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, 10},
+	}
+	for _, testInput := range tests {
+		t.Run(fmt.Sprintf("TestRecursive: %d", testInput.expected), func(t *testing.T) {
+			got := sum(testInput.input)
+			if got != testInput.expected {
+				t.Fatalf("expected %d, got %d", testInput.expected, got)
+			}
+		})
+	}
+}
